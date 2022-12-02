ed2b86a96cb5d421adbbc37db4842b61278b1902
mudokja@gmail.com <mudokja@gmail.com>
2022-12-02 14:02:52 +0900

    update: babel warring fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,31 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+const securityHeaders = [
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block'
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN'
+  }
+]
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  },
+  
 }
 
 module.exports = nextConfig
