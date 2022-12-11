bbe670df5ee29d188e723b42787473709cfdb7ff
mudokja@gmail.com <mudokja@gmail.com>
2022-12-11 17:43:52 +0900

    feat:add bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const path = require('path')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+module.exports = withBundleAnalyzer({})
 const securityHeaders = [
   {
     key: 'X-XSS-Protection',
