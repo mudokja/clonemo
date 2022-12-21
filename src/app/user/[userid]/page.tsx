7fd7cbb363ef023495e241a09dc44585eb52655c
mudokja@gmail.com <mudokja@gmail.com>
2022-12-22 03:54:30 +0900

    test?: env 문제?

--- src/app/user/[userid]/page.tsx
+++ src/app/user/[userid]/page.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 
 async function getArtist(userid: any) {
-  const vapi=process.env.V_API
-  const res = await fetch(`/api/${vapi}/user/${userid}`);
+  const res = await fetch(`/api/v1/user/${userid}`);
   return res.json();
 }
 
@@ -13,10 +12,10 @@ export default async function Page({params:{userid}}: any) :Promise<JSX.Element>
     //이상한 에러가 발생해서 병렬 데이터 가져오기 시도
     const [user] = await Promise.all([userData])
     return (
-      <>
+      <div>
       <p>유저정보로딩</p>
       <p>유저 아이디 :{user.id}</p>
       <p>유저 정보 {user.name}</p>
-      </>
+      </div>
     )
   }
